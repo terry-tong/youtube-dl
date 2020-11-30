40c0eabcfeecfdfd553eb90963c44f13081775ce
terry-tong <69437642+terry-tong@users.noreply.github.com>
2020-11-30 15:24:42 -0500

    Add files via upload

--- youtube-dl.py
+++ youtube-dl.py
@@ -1,12 +1,14 @@
 try:
     from pytube import YouTube
-    import tkinter
+    import tkinter as tk
+    import requests
+    from tkinter import messagebox
+
 except NameError:
     print("Error! Module(s) not found!")
 
 
-class Program(tkinter.Frame):
-
+class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -14,38 +16,54 @@ class Program(tkinter.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.create_link_entry()
-        self.create_audio_button()
-        self.create_video_button()
-        self.create_exit_button()
+        # Audio Button
+        self.audio = tk.Button(text="Audio",command=self.audio_download)
+        self.audio.place(x=200, y=300)
+        # Video Button
+        self.video = tk.Button(text="Video", command=self.video_download)
+        self.video.place(x=275, y=300)
+        # Link Input
+        url_x = 50
+        url_y = 150
+        self.url_textbox = tk.Entry(width=50)
+        self.url = tk.Label(text="URL:").place(x=url_x, y=url_y)
+        self.url_textbox.insert(0,"https://")
+        self.url_textbox.place(x=url_x+91, y=url_y)
+        # Output Directory
+        output_x = 50
+        output_y = 200
+        self.output_directory_textbox = tk.Entry(width=50)
+        self.output_directory = tk.Label(text="Output: ").place(x=output_x, y=output_y)
+        self.output_directory_textbox.insert(0, "C:/Output")
+        self.output_directory_textbox.place(x=output_x+91, y=output_y)
+        # Exit Button
+        self.exit = tk.Button(text="Exit", command=self.master.destroy)
+        self.exit.place(x=450,y=350)
 
-    def create_audio_button(self):
-        audio_button = tkinter.Button(text="Audio", command=self.audio_download)
-        audio_button.pack()
-
-    def create_video_button(self):
-        video_button = tkinter.Button(text="Video", command=self.video_download)
-        video_button.pack()
-
-    def create_exit_button(self):
-        exit_button = tkinter.Button(text="Exit", command=self.master.destroy)
-        exit_button.pack()
-
-    def create_link_entry(self):
-        self.link_entry = tkinter.Entry(width=75)
-        self.link_entry.pack()
+    def link_is_valid(self):
+        try:
+            link = requests.get(str(self.url_textbox.get()))
+            if "Video unavailable" in link.text:
+                messagebox.showerror(title="Error", message="Invalid Link! Please try again.")
+            else:
+                return True
+        except:
+            messagebox.showerror(title="Error", message="Invalid link! Please try again.")
 
     def audio_download(self):
-        video_url = str(self.link_entry.get())
-        YouTube(video_url).streams.filter(only_audio=True).order_by("abr").desc().first().download(output_dir)
+        if self.link_is_valid():
+            YouTube(self.url_textbox.get()).streams.filter(only_audio=True).order_by("abr").desc().first().download(self.output_directory_textbox.get())
+            messagebox.showinfo(title="Success", message="Audio successfully downloaded!")
 
     def video_download(self):
-        video_url = str(self.link_entry.get())
-        YouTube(video_url).streams.filter(subtype="mp4").order_by("resolution").desc().first().download(output_dir)
+        if self.link_is_valid():
+            YouTube(self.url_textbox.get()).streams.filter(subtype="mp4").order_by("resolution").desc().first().download(self.output_directory_textbox.get())
+            messagebox.showinfo(title="Success", message="Video successfully downloaded!")
 
-link = "https://www.youtube.com/watch?v=2lAe1cqCOXo&t"
-output_dir = "C:\Output"
-root = tkinter.Tk()
-root.geometry("500x500")
-window = Program(master=root)
-window.mainloop()
+
+test_link = "https://www.youtube.com/watch?v=2lAe1cqCOXo&t"
+root = tk.Tk()
+root.geometry("500x400")
+root.resizable(False,False)
+app = Application(master=root)
+app.mainloop()
